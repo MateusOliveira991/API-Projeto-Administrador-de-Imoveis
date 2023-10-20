e0f530c9ac469842b85dc51c50f2158d6376d5fb
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2023-10-20 14:06:26 -0300

    Feat: Implementação da classe InquilinoRepository

---
+++ proj-individual/src/main/java/br/com/api/projindividual/repositories/InquilinoRepository.java
@@ -0,0 +1,9 @@
+package br.com.api.projindividual.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.api.projindividual.entities.Inquilino;
+
+public interface InquilinoRepository extends JpaRepository<Inquilino, Integer>{
+
+}
