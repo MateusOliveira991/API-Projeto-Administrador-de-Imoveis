094f9405e3cc4bf5a8a617881348ba7bad15ac46
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2023-10-20 16:48:20 -0300

    Feat: Implementação da classe InquilinoService.

---
+++ proj-individual/src/main/java/br/com/api/projindividual/services/InquilinoService.java
@@ -0,0 +1,57 @@
+package br.com.api.projindividual.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.api.projindividual.entities.Inquilino;
+import br.com.api.projindividual.entities.Proprietario;
+import br.com.api.projindividual.repositories.InquilinoRepository;
+
+public class InquilinoService {
+	
+	@Autowired
+	InquilinoRepository inquilinoRepository;
+	
+	public Inquilino visualizarInquilino(Integer id) {
+		return inquilinoRepository.findById(id).get();
+	}
+	
+	public List<Inquilino>visualizarInquilinos(){
+		return inquilinoRepository.findAll();
+	}
+	public Inquilino registrarInquilino(Inquilino inquilino) {
+		return inquilinoRepository.save(inquilino);
+	}
+	
+	public Inquilino atualizarInquilino (Integer id, Inquilino inquilino) {
+		Inquilino registroAntigo=visualizarInquilino(id);
+		if(inquilino.getNome()!=null) {
+			registroAntigo.setNome(inquilino.getNome());
+		}
+		
+		if(inquilino.getTelefone()!=null){
+			registroAntigo.setTelefone(inquilino.getTelefone());	
+		}
+		
+		if(inquilino.getCpf() != null) {
+			registroAntigo.setCpf(inquilino.getCpf());
+		}
+		
+		if(inquilino.getEndereco()!=null){
+			registroAntigo.setEndereco(inquilino.getEndereco());	
+			}
+			
+		
+		registroAntigo.setId(id);
+		return inquilinoRepository.save(registroAntigo);
+		
+	}
+		
+		public void apagarInquilino(Integer id) {
+			Inquilino inquilinoDeletado = visualizarInquilino(id);
+			inquilinoRepository.delete(inquilinoDeletado);
+		}
+	}
+
+
