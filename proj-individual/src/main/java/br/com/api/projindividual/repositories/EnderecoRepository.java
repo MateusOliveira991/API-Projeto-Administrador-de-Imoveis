5ebab6c700d575faef73ba45c184a74773c73100
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2023-10-22 19:43:08 -0300

    Feat: Implementação da classe EnderecoRepositorry

---
+++ proj-individual/src/main/java/br/com/api/projindividual/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package br.com.api.projindividual.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.api.projindividual.entities.Endereco;
+
+
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
