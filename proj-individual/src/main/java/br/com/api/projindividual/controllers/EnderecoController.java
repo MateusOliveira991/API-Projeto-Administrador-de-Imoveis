e6ac26e5eb124eb9118fdfd3727b03cc964a864c
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2023-10-26 18:18:33 -0300

    Fear: Incluindo anotações

--- proj-individual/src/main/java/br/com/api/projindividual/controllers/EnderecoController.java
+++ proj-individual/src/main/java/br/com/api/projindividual/controllers/EnderecoController.java
@@ -14,26 +14,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.projindividual.entities.Endereco;
 import br.com.api.projindividual.services.EnderecoService;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping
+@RequestMapping ("/endereco")
 public class EnderecoController {
 	
 	@Autowired //fornece as instâncias de objetos necessárias,
 	EnderecoService enderecoService;
 	
+	@ApiOperation("Seleciona um endereço a partir do id")
 	@GetMapping("/visualizar-endereco/{id}")
 	public Endereco visualizarProprietario(@PathVariable Integer id) {
 		
 		return enderecoService.visualizarEndereco(id);
 	}
 	
+	@ApiOperation("Retorna uma lista com todos os endereços registrados")
 	@GetMapping("/visualizar-enderecos")
 	public List<Endereco>visualizarEnderecos(){
 		
 		return enderecoService.visualizarEnderecos();
 	}
 	
+	@ApiOperation("Registra um endereço a partir de cep e complemento")
 	@PostMapping ("/registrar-endereco")
 	
 	public Endereco registrarEndereco (@RequestBody Endereco endereco) {
@@ -41,10 +45,13 @@ public class EnderecoController {
 		return enderecoService.registrarEndereco(endereco);
 	}
 	
+	@ApiOperation("Atualiza um endereço selecionado por id")
 	@PutMapping("/atualizar-endereco/{id}")
 	public Endereco atualizarProprietario(@PathVariable Integer id, @RequestBody Endereco endereco) {
 		return enderecoService.atualizarEndereco(id, endereco);
 	}
+	
+	@ApiOperation("Deleta um endereço selecionado por id")
 	@DeleteMapping ("/apagar-endereco/{id}")
 	public void apagarProprietario(@PathVariable Integer id) {
 		
--- proj-individual/src/main/java/br/com/api/projindividual/entities/Endereco.java
+++ proj-individual/src/main/java/br/com/api/projindividual/entities/Endereco.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -15,6 +16,7 @@ public class Endereco {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@NotBlank
 	@Size(min=8, max=8)
 	private String cep;
 	private String logradouro;
